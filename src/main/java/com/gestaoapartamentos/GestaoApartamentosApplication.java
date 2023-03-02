030c8b3c97201a4a6ea8fac1b4b2b9c747a37750
jiuliogusmao <jiuliogusmao@gmail.com>
2023-03-02 18:52:24 -0300

    initial commit

---
+++ src/main/java/com/gestaoapartamentos/GestaoApartamentosApplication.java
@@ -0,0 +1,13 @@
+package com.gestaoapartamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoApartamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoApartamentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestaoapartamentos/GestaoApartamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestaoapartamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoApartamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
