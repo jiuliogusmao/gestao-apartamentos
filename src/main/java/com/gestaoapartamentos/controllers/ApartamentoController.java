a0842bd7f7e8935159b46cafb995c098e19ced98
jiuliogusmao <jiuliogusmao@gmail.com>
2023-03-05 08:08:35 -0300

    feat: endpoint cadastro apartamento

--- src/main/java/com/gestaoapartamentos/controllers/ApartamentoController.java
+++ src/main/java/com/gestaoapartamentos/controllers/ApartamentoController.java
@@ -1,8 +1,22 @@
 package com.gestaoapartamentos.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class ApartamentoController {
+import com.gestaoapartamentos.dto.ApartamentoDto;
+import com.gestaoapartamentos.services.ApartamentoService;
 
+@RestController
+@RequestMapping("api/v1/apartamento")
+public class ApartamentoController {
+	
+public ApartamentoService apartamentoService;
+	
+	@PostMapping("/cadastro")
+	public ResponseEntity<Object> cadastrarApartamento(@RequestBody ApartamentoDto apartamentoDto){
+		return apartamentoService.cadastrarApartamento(apartamentoDto);
+	}
 }
