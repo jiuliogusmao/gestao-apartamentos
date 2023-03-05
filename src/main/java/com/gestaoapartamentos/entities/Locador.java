02f6123251cef0493dc0abc51f920d3ebbec1fa0
jiuliogusmao <jiuliogusmao@gmail.com>
2023-03-05 09:22:47 -0300

    feat: entidade locador

---
+++ src/main/java/com/gestaoapartamentos/entities/Locador.java
@@ -0,0 +1,49 @@
+package com.gestaoapartamentos.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@Entity
+@Table(name = "tb_locador")
+public class Locador implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+	@Column(nullable = false)
+	private String cnpj;
+	
+	@Column(nullable = false)
+	private String endereco;
+	
+	@Column(nullable = false)
+	private String telefone;
+	
+	
+
+}
