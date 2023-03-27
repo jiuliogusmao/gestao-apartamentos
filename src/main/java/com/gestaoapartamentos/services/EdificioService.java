bd6135902559e9d0b660edc0328992a4022f91d4
jiuliogusmao <jiuliogusmao@gmail.com>
2023-03-27 19:45:27 -0300

    feat: endpoint para buscar edificio por nome

--- src/main/java/com/gestaoapartamentos/services/EdificioService.java
+++ src/main/java/com/gestaoapartamentos/services/EdificioService.java
@@ -18,45 +18,54 @@ import com.gestaoapartamentos.repositories.EdificioRepository;
 @Service
 public class EdificioService {
 
-	private final EdificioRepository edificioRepository;
-	
-	private final ApartamentoRepository apartamentoRepository;
+    private final EdificioRepository edificioRepository;
 
-	public EdificioService(EdificioRepository edificioRepository, ApartamentoRepository apartamentoRepository) {
-		this.edificioRepository = edificioRepository;
-		this.apartamentoRepository = apartamentoRepository;
-	}
+    private final ApartamentoRepository apartamentoRepository;
 
-	public ResponseEntity<Object> cadastrarEdificio(EdificioDto edificioDto) {
-		Optional<Edificio> edificioOptional = edificioRepository.findByNome(edificioDto.getNome());
-		if (edificioOptional.isPresent()) {
-			return ResponseEntity.status(HttpStatus.CONFLICT).body("Este edifício já está cadastrado.");
-		}
-		Edificio edificio = new Edificio();
-		BeanUtils.copyProperties(edificioDto, edificio);
-		edificioRepository.save(edificio);
+    public EdificioService(EdificioRepository edificioRepository, ApartamentoRepository apartamentoRepository) {
+        this.edificioRepository = edificioRepository;
+        this.apartamentoRepository = apartamentoRepository;
+    }
 
-		return ResponseEntity.status(HttpStatus.CREATED).body("Edifício cadastrado com sucesso!");
+    public ResponseEntity<Object> cadastrarEdificio(EdificioDto edificioDto) {
+        Optional<Edificio> edificioOptional = edificioRepository.findByNome(edificioDto.getNome());
+        if (edificioOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Este edifício já está cadastrado.");
+        }
+        Edificio edificio = new Edificio();
+        BeanUtils.copyProperties(edificioDto, edificio);
+        edificioRepository.save(edificio);
 
-	}
+        return ResponseEntity.status(HttpStatus.CREATED).body("Edifício cadastrado com sucesso!");
 
-	public ResponseEntity<Object> listarEdificios() {
-		List<Edificio> listaEdificio = edificioRepository.findAll();
-		return ResponseEntity.status(HttpStatus.OK).body(listaEdificio);
-	}
+    }
 
-	public ResponseEntity<Object> cadastrarApartamento(Long idEdificio, ApartamentoDto apartamentoDto){
-		Optional<Apartamento> apartamentoOptional = apartamentoRepository.findByNumeroApartamento(apartamentoDto.getNumeroApartamento());
-		if (apartamentoOptional.isPresent()) {
-			return ResponseEntity.status(HttpStatus.CONFLICT).body("Este apartamento já está cadastrado.");
-		}
-		Apartamento apartamento = new Apartamento();
-		BeanUtils.copyProperties(apartamentoDto, apartamento);
-		Edificio edificio = edificioRepository.getReferenceById(idEdificio);
-		apartamento.setEdificio(edificio);		
-		apartamentoRepository.save(apartamento);
-		edificio.addApartamento(apartamento);
-		
-		return ResponseEntity.status(HttpStatus.OK).body("Apartamento cadastrado com sucesso.");
-	}
+    public ResponseEntity<Object> listarEdificios() {
+        List<Edificio> listaEdificio = edificioRepository.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(listaEdificio);
+    }
+
+    public ResponseEntity<Object> buscarEdificioPorNome(String nome) {
+        Optional<Edificio> edificioOptional = edificioRepository.findByNome(nome);
+        if (edificioOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.OK).body(edificioOptional.get());
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Edifício não encontrado.");
+        }
+    }
+
+    public ResponseEntity<Object> cadastrarApartamento(Long idEdificio, ApartamentoDto apartamentoDto) {
+        Optional<Apartamento> apartamentoOptional = apartamentoRepository.findByNumeroApartamento(apartamentoDto.getNumeroApartamento());
+        if (apartamentoOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Este apartamento já está cadastrado.");
+        }
+        Apartamento apartamento = new Apartamento();
+        BeanUtils.copyProperties(apartamentoDto, apartamento);
+        Edificio edificio = edificioRepository.getReferenceById(idEdificio);
+        apartamento.setEdificio(edificio);
+        apartamentoRepository.save(apartamento);
+        edificio.addApartamento(apartamento);
+
+        return ResponseEntity.status(HttpStatus.OK).body("Apartamento cadastrado com sucesso.");
+    }
 }
